7feb455a8bc478d8fc01f5ccf13c19a3cf4c9588
Jacky <jacky2wong@gmail.com>
2021-02-10 21:00:18 +1100

    add return curl to write.py

--- vectorai/api/write.py
+++ vectorai/api/write.py
@@ -1,7 +1,7 @@
 import requests
 from typing import Dict, List
 from .read import ViReadAPIClient
-from .utils import retry, return_response
+from .utils import retry, return_curl_or_response
 from ..errors import APIError, CollectionNameError
 
 class ViWriteAPIClient(ViReadAPIClient):
@@ -25,7 +25,7 @@ class ViWriteAPIClient(ViReadAPIClient):
             raise CollectionNameError("Collection names must be lower case A-Z and less than 240 characters")
     
     @retry()
-    def create_collection_from_document(self, collection_name: str, document: dict):
+    def create_collection_from_document(self, collection_name: str, document: dict, return_curl: bool=False, **kwargs):
         """
 Creates a collection by infering the schema from a document
 
@@ -38,18 +38,21 @@ Args:
 		A Document is a JSON-like data that we store our metadata and vectors with. For specifying id of the document use the field '\_id', for specifying vector field use the suffix of '\_vector\_'
 """
         self._typecheck_collection_name(collection_name)
-        return requests.post(
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "document": document,
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/project/create_collection_from_document".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-                "document": document,
-            },
-        ).json()
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def bulk_insert_and_encode(self, collection_name: str, docs: list, models: dict):
+    def bulk_insert_and_encode(self, collection_name: str, docs: list, models: dict, return_curl: bool=False, **kwargs):
         """
             Client-side encoding of documents to improve speed of inserting. This removes
             the step of retrieving the vectors and can be useful to accelerate the encoding
@@ -60,38 +63,50 @@ Args:
         # are in the list below.
         for k, v in models.items():
             assert v in ['text', 'audio', 'image']
-        return requests.post(
+
+        params={
+            'username' : self.username,
+            'api_key' : self.api_key,
+            'collection_name': collection_name,
+            'documents' : docs,
+            'models' : models
+        }
+        params.update(kwargs)
+        response = requests.post(
             url='{}/collection/bulk_insert_and_encode'.format(self.url), 
-            json={
-                'username' : self.username,
-                'api_key' : self.api_key,
-                'collection_name': collection_name,
-                'documents' : docs,
-                'models' : models
-        }).json()
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def _create_collection(self, collection_name: str, collection_schema: Dict = {}):
-        return requests.post(
+    def _create_collection(self, collection_name: str, collection_schema: Dict = {}, return_curl: bool=False, **kwargs):
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "collection_schema": collection_schema,
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/project/create_collection".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-                "collection_schema": collection_schema,
-            },
-        ).json()
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
+
 
     @retry()
-    def _delete_collection(self, collection_name: str):
-        return requests.get(
+    def _delete_collection(self, collection_name: str, return_curl: bool=False, **kwargs):
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+        }
+        params.update(kwargs)
+        response = requests.get(
             url="{}/project/delete_collection".format(self.url),
-            params={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-            },
-        ).json()
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     # def replicate_collection(self, collection_name: str, new_collection_name: str):
     #     return requests.get(
@@ -106,7 +121,7 @@ Args:
 
     @retry()
     def bulk_insert(self, collection_name: str, documents: List, insert_date: bool=True, overwrite: bool=True, 
-    quick: bool=False):
+    quick: bool=False, return_curl: bool=False, **kwargs):
         """
 Insert multiple documents into a Collection
 When inserting the document you can specify your own id for a document by using the field name **"\_id"**. 
@@ -125,21 +140,26 @@ Args:
     quick:
         If True, skips collection schema checks
 """
-        return requests.post(
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "documents": documents,
+            "insert_date": insert_date,
+            "overwrite" : overwrite,
+            "quick": quick
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/collection/bulk_insert".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-                "documents": documents,
-                "insert_date": insert_date,
-                "overwrite" : overwrite,
-                "quick": quick
-            },
-        ).json()
+            json=params
+        )
+        return return_curl_or_response(response)
     
     @retry()
-    def insert(self, collection_name: str, document: Dict, insert_date: bool=True, overwrite: bool=True):
+    def insert(self, collection_name: str, document: Dict, insert_date: bool=True, overwrite: bool=True,
+    return_curl: bool=False, **kwargs):
         """
 Insert a document into a Collection
 When inserting the document you can specify your own id for a document by using the field name **"\_id"**. 
@@ -156,52 +176,62 @@ Args:
 	overwrite:
 		Whether to overwrite document if it exists.
 """
-        return requests.post(
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "document": document,
+            "insert_date": insert_date,
+            "overwrite" : overwrite
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/collection/insert".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-                "document": document,
-                "insert_date": insert_date,
-                "overwrite" : overwrite
-            },
-        ).json()
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
     
     @retry()
-    def _edit_document(self, collection_name: str, edits: Dict, document_id: str):
-        return requests.post(
+    def _edit_document(self, collection_name: str, edits: Dict, document_id: str, return_curls: bool=False, **kwargs):
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "edits": edits,
+            "document_id": document_id,
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/collection/edit_document".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-                "edits": edits,
-                "document_id": document_id,
-            },
-        ).json()
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def bulk_edit_document(self, collection_name: str, documents: List[Dict]):
+    def bulk_edit_document(self, collection_name: str, documents: List[Dict], return_curl: bool=False, **kwargs):
         """
         Edits documents by providing a key value pair of fields you are adding or changing, make sure to include the "_id" in the documents.
         Args:
             collection_name: Name of collection
             documents: A list of documents. Document is a JSON-like data that we store our metadata and vectors with. For specifying id of the document use the field '_id', for specifying vector field use the suffix of '_vector_'
         """
-        return return_response(
-            requests.post(
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "documents": documents
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/collection/bulk_edit_document".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-                "documents": documents
-            }
-        ))
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def delete_by_id(self, collection_name: str, document_id: str):
+    def delete_by_id(self, collection_name: str, document_id: str, return_curl: bool=False, **kwargs):
         """
 Delete a document in a Collection by its id
     
@@ -211,15 +241,18 @@ Args:
 	collection_name:
 		Name of Collection
 """
-        return requests.get(
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "document_id": document_id,
+        }
+        params.update(kwargs)
+        response = requests.get(
             url="{}/collection/delete_by_id".format(self.url),
-            params={
-                "username": self.username,
-                "api_key": self.api_key,
-                "collection_name": collection_name,
-                "document_id": document_id,
-            },
-        ).json()
+            params=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
     def publish_aggregation(
@@ -232,6 +265,8 @@ Args:
         date_field: str = "insert_date_",
         refresh_time: int = 30,
         start_immediately: bool = True,
+        return_curl: bool=False,
+        **kwargs
     ):
         """
 Publishes your aggregation query to a new collection
@@ -256,24 +291,28 @@ Args:
 	start_immediately:
 		Whether to start the published aggregation immediately
 """
-        return requests.post(
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "aggregation_query": aggregation_query,
+            "source_collection": collection_name,
+            "dest_collection": aggregated_collection_name,
+            "aggregation_name": aggregation_name,
+            "description": description,
+            "date_field": date_field,
+            "refresh_time": "{}s".format(refresh_time),
+            "start_immediately": start_immediately,
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/collection/publish_aggregation".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "aggregation_query": aggregation_query,
-                "source_collection": collection_name,
-                "dest_collection": aggregated_collection_name,
-                "aggregation_name": aggregation_name,
-                "description": description,
-                "date_field": date_field,
-                "refresh_time": "{}s".format(refresh_time),
-                "start_immediately": start_immediately,
-            },
-        ).json()
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def start_aggregation(self, aggregation_name: str):
+    def start_aggregation(self, aggregation_name: str, return_curl: bool=False, **kwargs):
         """
 Start your published aggregation
 Start or resume your published aggregation. The published aggregation can be stopped with /stop_aggregation.
@@ -282,17 +321,21 @@ Args:
 	aggregation_name:
 		The name for the published scheduled aggregation
 """
-        return requests.get(
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "aggregation_name": aggregation_name,
+        }
+        params.update(kwargs)
+        response = requests.get(
             url="{}/collection/start_aggregation".format(self.url),
-            params={
-                "username": self.username,
-                "api_key": self.api_key,
-                "aggregation_name": aggregation_name,
-            },
-        ).json()
+            params=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def stop_aggregation(self, aggregation_name: str):
+    def stop_aggregation(self, aggregation_name: str, return_curl: bool=False, **kwargs):
         """
 Stop your published aggregation
 Stop/pause your published aggregation. The published aggregation can be resumed/started with /start_aggregation.
@@ -301,17 +344,21 @@ Args:
 	aggregation_name:
 		The name for the published scheduled aggregation
 """
-        return requests.get(
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "aggregation_name": aggregation_name,
+        }
+        params.update(kwargs)
+        response = requests.get(
             url="{}/collection/stop_aggregation".format(self.url),
-            params={
-                "username": self.username,
-                "api_key": self.api_key,
-                "aggregation_name": aggregation_name,
-            },
-        ).json()
+            params=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def delete_published_aggregation(self, aggregation_name: str):
+    def delete_published_aggregation(self, aggregation_name: str, return_curl: bool=False):
         """
 Delete a published aggregation and collection
 Delete a published aggregation and the associated collection it creates.
@@ -320,17 +367,20 @@ Args:
 	aggregation_name:
 		The name for the published scheduled aggregation
 """
-        return requests.get(
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "aggregation_name": aggregation_name,
+        }
+        response = requests.get(
             url="{}/collection/delete_published_aggregation".format(self.url),
-            params={
-                "username": self.username,
-                "api_key": self.api_key,
-                "aggregation_name": aggregation_name,
-            },
-        ).json()
+            params=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
 
     @retry()
-    def join_collections(self, join_query: dict, joined_collection_name:str):
+    def join_collections(self, join_query: dict, joined_collection_name:str, return_curl: bool=False, **kwargs):
         """
 Join collections with a query
 Perform a join query on a whole collection and write the results to a new collection. We currently only support left joins.
@@ -341,12 +391,16 @@ Args:
 	joined_collection_name:
 		Name of the new collection that contains the joined results
 """
-        return requests.post(
+
+        params={
+            "username": self.username,
+            "api_key": self.api_key,
+            "join_query": join_query,
+            "joined_collection_name": joined_collection_name,
+        }
+        params.update(kwargs)
+        response = requests.post(
             url="{}/collection/join_collections".format(self.url),
-            json={
-                "username": self.username,
-                "api_key": self.api_key,
-                "join_query": join_query,
-                "joined_collection_name": joined_collection_name,
-            },
-        ).json()
+            json=params
+        )
+        return return_curl_or_response(response, 'json', return_curl)
