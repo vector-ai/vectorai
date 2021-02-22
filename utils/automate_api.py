5ab1664bdd36990300c3944823ae62a2a9e682f6
Jacky <jacky2wong@gmail.com>
2021-02-22 21:59:49 +1100

    Add api automation

---
+++ utils/automate_api.py
@@ -0,0 +1,16 @@
+from openapi_to_sdk.sdk_automation import PythonSDKBuilder
+sdk = PythonSDKBuilder(
+    url="https://api.vctr.ai",
+    inherited_properties=['username', 'api_key'],
+    decorators=['retry()'],
+)
+sdk.to_python_file(
+    class_name="_ViAPIClient", 
+    filename='vectorai/api/api.py',
+    import_strings=['import requests', 'from vectorai.api.utils import retry'], 
+    internal_functions=[
+        "list_collections",
+        "create_collection",
+        "search"
+    ]
+)
