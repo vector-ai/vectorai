78303c868efcb583a4e6674dbb8cdbedc4ecbeef
Jacky <jacky2wong@gmail.com>
2021-03-03 12:32:15 +1100

    add constructor

--- vectorai/api/client.py
+++ vectorai/api/client.py
@@ -6,4 +6,7 @@ from .image import *
 from .dimensionality_reduction import *
 
 class ViAPIClient(_ViAPIClient, ViDimensionalityReductionClient, ViTextClient, ViAudioClient, ViImageClient, ViClusterClient):
-    pass
+    def __init__(self, username, api_key, url="https://api.vctr.ai"):
+        self.username = username
+        self.api_key = api_key
+        self.url = url
