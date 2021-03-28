e30011830a49a35aa22c5772b86f2cc86a01923b
Jacky <jacky2wong@gmail.com>
2021-03-28 15:42:12 +1100

    Update API

--- utils/automate_api.py
+++ utils/automate_api.py
@@ -1,14 +1,25 @@
 if __name__=="__main__":
     import os
     from openapi_to_sdk.sdk_automation import PythonSDKBuilder
+
+    url="https://vectorai-development-api.azurewebsites.net"
+    url="https://vectorai-development-api.azurewebsites.net"
+    # url = "https://api.vctr.ai"
     sdk = PythonSDKBuilder(
-        url='https://vecdb-aueast-api.azurewebsites.net',
+        url=url,
+        # url="https://vectorai-development-api.azurewebsites.net",
+        # url='https://vecdb-aueast-api.azurewebsites.net',
         inherited_properties=['username', 'api_key', 'url'],
         decorators=[
-            'retry()', 
+            'retry()',
             "return_curl_or_response('json')"],
-        override_param_defaults={'min_score': None, 'cursor': None, 'url': 'https://vecdb-aueast-api.azurewebsites.net',# 'sort': False
-        },
+        override_param_defaults=dict(
+            min_score=None,
+            cursor=None,
+            # url='https://vecdb-aueast-api.azurewebsites.net',
+            url=url,
+            sort=False)
+        ,
         internal_functions=[
             "list_collections",
             "create_collection",
@@ -18,12 +29,12 @@ if __name__=="__main__":
         ],
     )
     sdk.to_python_file(
-        class_name="_ViAPIClient", 
+        class_name="_ViAPIClient",
         filename='vectorai/api/api.py',
-        import_strings=['import requests', 'from vectorai.api.utils import retry, return_curl_or_response'], 
+        import_strings=['import requests', 'from vectorai.api.utils import retry, return_curl_or_response'],
         include_response_parsing=False,
     )
 
-    from vectorai.api import ViAPIClient
-    vi = ViAPIClient(os.environ['VI_USERNAME'], os.environ['VI_API_KEY'])
+    from vectorai.api.api import _ViAPIClient
+    vi = _ViAPIClient(os.environ['VI_USERNAME'], os.environ['VI_API_KEY'], url=url)
     print(vi._list_collections())
