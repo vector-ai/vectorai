bf995ecf86f3c292e4928fc547a3cb8c98358fa7
Jacky <jacky2wong@gmail.com>
2021-04-10 01:20:48 +1000

    Add reference to delete collection

--- tests/utils.py
+++ tests/utils.py
@@ -10,7 +10,7 @@ class TempClient:
             self.collection_name = collection_name
         # elif isinstance(client, ViCollectionClient):
         #     self.collection_name = self.client.collection_name
-        
+
     def teardown_collection(self):
         if self.collection_name in self.client.list_collections():
             time.sleep(2)
@@ -18,11 +18,11 @@ class TempClient:
                 self.client.delete_collection(self.collection_name)
             elif isinstance(self.client, ViCollectionClient):
                 self.client.delete_collection()
-    
+
     def __enter__(self):
         self.teardown_collection()
         return self.client
-    
+
     def __exit__(self, *exc):
         self.teardown_collection()
 
@@ -35,24 +35,24 @@ class TempClientWithDocs(TempClient):
         if hasattr(self.client, 'collection_name'):
             self.collection_name = collection_name
         else:
-            if collection_name is None: 
+            if collection_name is None:
                 collection_name = self.generate_random_collection_name()
             self.collection_name = collection_name
             self.client.collection_name = collection_name
         self.num_of_docs = num_of_docs
         self.teardown_collection()
-        self.client.insert_documents(self.collection_name, 
+        self.client.insert_documents(self.collection_name,
         self.client.create_sample_documents(self.num_of_docs))
-    
+
     def generate_random_collection_name(self):
         return self.generate_random_string(20)
-    
+
     def generate_random_string(self, num_of_letters):
         letters = string.ascii_lowercase
-        return ''.join(random.choice(letters) for i in range(num_of_letters))
+        return '_delete_collection_'.join(random.choice(letters) for i in range(num_of_letters))
 
     def __enter__(self):
         # self.teardown_collection()
-        # self.client.insert_documents(self.collection_name, 
+        # self.client.insert_documents(self.collection_name,
         # self.client.create_sample_documents(self.num_of_docs))
         return self.client
